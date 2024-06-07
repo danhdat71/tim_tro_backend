60d4d7866f2239236056055b3fb8ffce993f184a
Bap-datd <datd@bap.jp>
2024-06-07 13:29:25 +0700

    Create notification

---
+++ database/migrations/2024_06_07_110826_create_table_notifications.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('description', 255)->nullable()->default(null);
+            $table->tinyInteger('status')->default(0)->comment('0: unread, 1: read');
+            $table->string('link')->nullable()->default(null);
+            $table->foreignId('user_id')->nullable()->default(null)->index();
+            $table->dateTime('sent_at')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notifications');
+    }
+};
